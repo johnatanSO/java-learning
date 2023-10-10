891dcc7f4909090930ea3c74751d0d6f4fe8d3f1
johnatanSO <jonystrit@gmail.com>
2023-10-10 11:31:55 -0300

    :sparkles: [FEAT] - Creating repository.

--- src/main/java/br/com/johnatanso/todolist/user/UserController.java
+++ src/main/java/br/com/johnatanso/todolist/user/UserController.java
@@ -1,19 +1,26 @@
 package br.com.johnatanso.todolist.user;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/")
-    public String getUserInfo() {
-        return "User info";
-    }
-
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
-        return userModel;
+    public ResponseEntity<UserModel> createUser(@RequestBody UserModel userModel) {
+
+        UserRepository userRepository = new UserRepository();
+
+        UserModel newUser = userRepository.createUser(userModel);
+
+        System.out.print(newUser);
+
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 }
+
--- src/main/java/br/com/johnatanso/todolist/user/UserModel.java
+++ src/main/java/br/com/johnatanso/todolist/user/UserModel.java
@@ -1,7 +1,17 @@
 package br.com.johnatanso.todolist.user;
 
+import java.util.UUID;
+
 public class UserModel {
     public String username;
-    public String name;
+    public String email;
     public String password;
+    public UUID _id;
+
+    public UserModel(String username, String email, String password) {
+        this._id = UUID.randomUUID();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    };
 }
